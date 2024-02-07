da3b54643a0955dba3a7cd9785e1d4e08143bff1
苗木蛤 <2791592045@qq.com>
2024-02-07 23:46:58 +0800

    文件传输功能

--- src/components/home/Trans.vue
+++ src/components/home/Trans.vue
@@ -5,7 +5,7 @@
         <el-upload
             class="upload-demo"
             drag
-            action="https://jsonplaceholder.typicode.com/posts/"
+            action="http://localhost:9090/temp/upload"
             multiple>
           <i class="el-icon-upload"></i>
           <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
